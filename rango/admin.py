8fb3a8ba5ea9148fb5f9cd71c4a341b2a6e109b7
Samuel Dolt <samuel@MBP-de-Samuel.lan>
2014-10-19 20:50:12 +0200

    Added admin interface for Categories and Pages

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+admin.site.register(Category)
+
+
+class PageAdmin(admin.ModelAdmin):
+    fields = ['title', 'url', 'category', 'views']
+    readonly_fields = ['views']
+
+    list_display = fields
+
+admin.site.register(Page, PageAdmin)
